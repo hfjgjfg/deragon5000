f956090d6db576ca77cb4b2468594142408f67c4
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-03-16 13:06:44 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -234,7 +234,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {199433012},--Sudo users
+    sudo_users = {199433012,150575718},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
